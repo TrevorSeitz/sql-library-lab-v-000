3a84feaaa0d76b8c7b21315b2630524495209a97
Trevor Seitz <trevinla@gmail.com>
2018-04-17 14:58:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,12 +1,12 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books
+  INNER JOIN series_id
+  ON series.id = series_id
+  WHERE id=(SELECT MIN(id) FROM series);"
 end
 
-SELECT books.title, books.year
-FROM books
-INNER JOIN series_id
-ON series.id = series_id
-WHERE id=(SELECT MIN(id) FROM series);
+
 
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
