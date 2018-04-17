264efdb08edbb26326f5c994087f996bba4e5a89
Trevor Seitz <trevinla@gmail.com>
2018-04-17 14:54:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 SELECT books.title, books.year
-FROM 
+FROM projects
 SET species = "cylon", species = "Martian"
 WHERE id=(SELECT MAX(id) FROM characters);
 
