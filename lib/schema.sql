f1e8e35f319e45c57e738d3f175885062f34636f
Trevor Seitz <trevinla@gmail.com>
2018-04-17 14:06:21 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
+
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER
+);
+
+CREATE TABLE character (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT
+);
