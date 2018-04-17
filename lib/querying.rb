601a4c73c3220751ae2ae1b2b8b5fa0b6a2f6e85
Trevor Seitz <trevinla@gmail.com>
2018-04-17 15:20:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  WHERE motto = MAX(length(motto));
+  ORDER BY length(motto)
+  LIMIT 1;
   "
   # SELECT MAX(LENGTH(motto)) FROM characters;
 end
