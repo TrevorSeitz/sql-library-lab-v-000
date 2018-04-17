a0ab672ea681c3e10bc6b45e603a3a21eb280c13
Trevor Seitz <trevinla@gmail.com>
2018-04-17 15:04:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   INNER JOIN series
   ON series.id = series_id
-  WHERE id=(SELECT MIN(id) FROM series);"
+  WHERE series.id=(SELECT MIN(id) FROM series);"
 end
 
 
