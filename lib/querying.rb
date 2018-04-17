a7d9732349a58e4db0307ad33d6d0ca6f4a5618d
Trevor Seitz <trevinla@gmail.com>
2018-04-17 15:39:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_value_and_count_of_most_prolific_species
   COUNT(species) ASC
   FROM characters
   GROUP BY species
-  LIMIT 1;"
+  ;"
 end
 
 def select_name_and_series_subgenres_of_authors
